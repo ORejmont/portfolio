0481960b195f4c5f77f9ae837b1cd5d3771827fd
OndrejRejmont <54508579+ORejmont@users.noreply.github.com>
2021-04-23 22:43:34 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,25 @@
+// Menu-toggle button
+
+$(document).ready(function() {
+    $(".menu-icon").on("click", function() {
+        $("nav ul").toggleClass("showing");
+    });
+});
+
+// Scrolling Effect
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop()) {
+        $('nav').addClass('black');
+    }
+
+    else {
+        $('nav').removeClass('black');
+    }
+})
+
+// HAMBURGER    
+
+$("#wrapper").click( function() {
+	$(".menu").toggleClass("close");
+});
